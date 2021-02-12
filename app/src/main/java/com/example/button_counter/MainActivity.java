8cf501cb186c85c2addd6d67c985174d97be34b6
Collin Bauer <collinlbauer@gmail.com>
2021-02-11 19:39:04 -0500

    wow

--- app/src/main/java/com/example/button_counter/MainActivity.java
+++ app/src/main/java/com/example/button_counter/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.button_counter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,8 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int counter = 0;
     private TextView counterView;
 
+    private int doge_chance;
+    private TextView doge;
+    private Random rng;
+    private ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        rng = new Random();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -31,6 +41,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonDec.setOnClickListener(this);
         buttonRes = findViewById(R.id.buttonRes);
         buttonRes.setOnClickListener(this);
+
+        doge_chance = 10 + rng.nextInt(7);
+        doge = findViewById(R.id.doge);
+        constraintLayout = findViewById(R.id.parent_layout);
+
     }
 
     @Override
@@ -40,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if(id == R.id.buttonInc) {
             counter++;
+            doge_chance--;
         }
         else if(id == R.id.buttonDec) {
             if (counter > 0) counter--;
@@ -47,8 +63,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if(id == R.id.buttonRes) {
             counter = 0;
         }
+
         counterView.setText(String.valueOf(counter));
+        if(doge_chance <= 0) {
+            triggerDoge();
+        }
+        else {
+            doge.setText("");
+        }
     }
+
+    private void triggerDoge(){
+        String[] dogeArray = getResources().getStringArray(R.array.doge);
+        doge.setRotation(rng.nextInt(100) - 50);
+
+        doge.setText(dogeArray[rng.nextInt(dogeArray.length)]);
+        ConstraintSet newSet = new ConstraintSet();
+        newSet.clone(constraintLayout);
+        newSet.setHorizontalBias(doge.getId(), (float) (0.1 + rng.nextFloat() * 0.8));
+        newSet.setVerticalBias(doge.getId(), (float) (0.6 + rng.nextFloat() * 0.3));
+        newSet.applyTo(constraintLayout);
+        doge_chance = 10 + rng.nextInt(7);
+    }
+
 
 
 
